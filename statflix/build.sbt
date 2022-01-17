9a37d2e1c2261b0ddd0574308727b8b3577e8570
unauo <ozturk.asli@metu.edu.tr>
2022-01-17 14:11:51 +0300

    add kaggle data and import codes, update scala version

--- statflix/build.sbt
+++ statflix/build.sbt
@@ -2,7 +2,7 @@ name := "statflix"
 
 version := "0.1"
 
-scalaVersion := "2.12.5"
+scalaVersion := "2.12.15"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "3.1.2"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.2"
--- statflix/src/main/scala/tr/edu/metu/ceng/statflix/Run.scala
+++ statflix/src/main/scala/tr/edu/metu/ceng/statflix/Run.scala
@@ -1,9 +1,27 @@
 package tr.edu.metu.ceng.statflix
 
+import org.apache.spark.sql.SparkSession
+
 object Run {
 
   def main(args: Array[String]): Unit = {
-    println("statflix")
+    val spark = SparkSession.builder().appName("StatFlix").config("spark.master", "local[*]").getOrCreate()
+    // show_id,type,title,director,cast,produced_by,date_added,release_year,viewer_rating,duration,listed_in,description
+    val kaggleData = spark.read
+      .format("csv")
+      .option("delimiter", ",")
+      .option("header", "true")
+      .load("data/netflix_titles.csv")
+
+    kaggleData.show(10)
+
+    val unogsData = spark.read
+      .format("json")
+      .option("multiline","true")
+      .json("data/unogs-data-formatted.json")
+
+    unogsData.show(10)
+    //
 
   }
 
